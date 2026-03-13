b722643d7fafd428100095f58e9e08b9a980ddbb
Milan Fox <milan.fox@linuxmail.org>
2026-03-13 23:16:39 +0100

    feat(app): integrate storyblok bridge

---
+++ app/pages/[...slug].vue
@@ -0,0 +1,9 @@
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+const slug = (route.params.slug as string[]).join('/');
+const story = await useStoryblok(slug, { version: 'published' });
+</script>
